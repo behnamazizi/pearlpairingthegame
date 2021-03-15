4219a6ca3b2436ddf4df3ce91070c2286ce6c230
Behnam Azizi <bh.azizi@gmail.com>
2021-03-15 20:13:09 +0330

    update SW

--- assets/script.js
+++ assets/script.js
@@ -10,34 +10,16 @@ let ww, wh, timer_0, timer_1, timer_2,
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js', {
-            scope: '/pearlpairingthegame/'
-        }).then(function (reg) {
-
-            if (reg.installing) {
-                console.log('Service worker installing');
-            } else if (reg.waiting) {
-                console.log('Service worker installed');
-            } else if (reg.active) {
-                console.log('Service worker active');
-            }
-
-        }).catch(function (error) {
-            // registration failed
-            console.log('Registration failed with ' + error);
-        });
+        navigator.serviceWorker.register('/sw.js')
+            .then(reg => console.log('service worker registered'))
+            .catch(err => console.log('service worker not registered', err));
     }
-
-
-
-
-
 
     timer_2 = window.requestAnimationFrame(() => {
         getComputedStyle(document.body).getPropertyValue("width").slice(0, -2) != 0 ? setup() : ''
     })
+
 })
 window.addEventListener('resize', () => setup())
 
@@ -164,27 +146,27 @@ const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  addBtn.style.display = 'block';
+    e.preventDefault();
+    deferredPrompt = e;
+    addBtn.style.display = 'block';
 
-  addBtn.addEventListener('click', () => {
-    // addBtn.style.display = 'none';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
+    addBtn.addEventListener('click', () => {
+        // addBtn.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
     });
-  });
 });
 
 window.addEventListener('appinstalled', () => {
     // addBtn.style.display = 'none';
-    document.querySelector('#additional').innerHTML+= `
+    document.querySelector('#additional').innerHTML += `
     #start{
         grid-template-columns: repeat(3, 1fr) .5fr 1fr;
     }
@@ -194,5 +176,4 @@ window.addEventListener('appinstalled', () => {
     `
     deferredPrompt = null;
     console.log('PWA was installed');
-  });
-  
+});
--- sw.js
+++ sw.js
@@ -1,27 +1,51 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('v1.3').then((cache) => cache.addAll([
-            '/pearlpairingthegame/',
-            '/pearlpairingthegame/index.html',
-            '/pearlpairingthegame/assets/reset.css',
-            '/pearlpairingthegame/assets/style.css',
-            '/pearlpairingthegame/assets/script.js',
-            '/pearlpairingthegame/assets/fonts/Bungee-Regular.woff2',
-            '/pearlpairingthegame/assets/images/ball-0-b.svg',
-            '/pearlpairingthegame/assets/images/ball-0-sh.svg',
-            '/pearlpairingthegame/assets/images/ball-1-b.svg',
-            '/pearlpairingthegame/assets/images/ball-1-sh.svg',
-            '/pearlpairingthegame/assets/images/ball-2-b.svg',
-            '/pearlpairingthegame/assets/images/ball-2-sh.svg',
-            '/pearlpairingthegame/assets/images/base-0.svg',
-            '/pearlpairingthegame/assets/images/base-1.svg',
-            '/pearlpairingthegame/assets/images/icon.svg',
-        ]))
-    )
+const CacheName = 'v1.5';
+const assets = [
+    '/pearlpairingthegame/',
+    '/pearlpairingthegame/index.html',
+    '/pearlpairingthegame/assets/reset.css',
+    '/pearlpairingthegame/assets/style.css',
+    '/pearlpairingthegame/assets/script.js',
+    '/pearlpairingthegame/assets/images/ball-0-b.svg',
+    '/pearlpairingthegame/assets/images/ball-0-sh.svg',
+    '/pearlpairingthegame/assets/images/ball-1-b.svg',
+    '/pearlpairingthegame/assets/images/ball-1-sh.svg',
+    '/pearlpairingthegame/assets/images/ball-2-b.svg',
+    '/pearlpairingthegame/assets/images/ball-2-sh.svg',
+    '/pearlpairingthegame/assets/images/base-0.svg',
+    '/pearlpairingthegame/assets/images/base-1.svg',
+    '/pearlpairingthegame/assets/images/icon.svg',
+    '/pearlpairingthegame/assets/fonts/Bungee-Regular.woff2',
+];
+
+// install event
+self.addEventListener('install', evt => {
+  console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(CacheName).then((cache) => {
+      console.log('caching...');
+      cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
-    );
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
